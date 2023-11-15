d197cf1e7a5f482c574a4d356d6abcfdfa062760
Vinicius Lelé <vinicius.a.lele@hotmail.com>
2023-11-15 11:38:59 -0300

    changes:colocado rota para api

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,7 +12,12 @@ use App\Controllers\Clinic;
 
 $routes->get('/',[Pacientes::class,'index']);
 $routes->get('/new', [Pacientes::class, 'new']);
-$routes->post('/', [Pacientes::class, 'create']);
+$routes->post('/pacientes/store', [Pacientes::class, 'create']);
 $routes->post('/update', [Pacientes::class, 'update']);
 $routes->get('/(:segment)', [Pacientes::class, 'delete']);
 $routes->get('/edit/(:segment)', [Pacientes::class, 'show']);
+
+$routes->group('api', static function ($routes) {
+    $routes->post('xxx', [Pacientes::class, 'create']);
+});
+
